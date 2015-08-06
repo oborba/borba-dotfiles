58b2e3c3874a529ecbcec8586865459f02d76b37
Diego Alves <diego.alves@locaweb.com.br>
2015-08-06 19:21:32 -0300

    Iterate thought files to backup

--- install.sh
+++ install.sh
@@ -4,15 +4,23 @@ set -eu
 
 TMUX_CONF=~/.tmux.conf
 BASHRC=~/.bashrc
+TESTE=~/teste_um.txt
+TESTE_DOIS=~/teste_dois.txt
+
+DOT_FILES_TO_BACKUP="$TESTE $TESTE_DOIS"
 
 BackupOldDotFile () {
   if [ -f $1  ]
   then
-    echo "Renaming $1 to .old$1"
-    mv $1 ~/old.$1
+    echo "Renaming $1 to $1.old"
+    mv $1 $1.old
   else
     echo "Don't exists, $1"
   fi
 }
 
-BackupOldDotFile $TMUX_CONF
+for i in $DOT_FILES_TO_BACKUP
+do
+  BackupOldDotFile "$i"
+done
+
