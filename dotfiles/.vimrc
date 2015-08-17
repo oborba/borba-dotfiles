1ecd45ea6fd5477e2c2d90483283fc5c924c76e5
Diego Alves <diego.oborba@gmail.com>
2015-08-16 21:58:42 -0300

    Adjust script

---
+++ dotfiles/.vimrc
@@ -0,0 +1,7 @@
+set tabstop=2
+set shiftwidth=2
+set expandtab
+
+syntax on
+
+set number
--- install.sh
+++ install.sh
@@ -2,18 +2,15 @@
 
 set -u
 
-TMUX_CONF=~/.tmux.conf
-BASHRC=~/.bashrc
-
-DOT_FILES_TO_BACKUP="$TMUX_CONF $BASHRC"
+DOT_FILES_TO_BACKUP=$(ls -a dotfiles/ | grep .[a-z]\w*)
 
 DATE=`date +%Y%m%d%H%M%S`
 
 BackupOldDotFile () {
-  if [ -f $1  ]
+  if [ -f ~/$1  ]
   then
     echo "Renaming $1 to $1.$DATE"
-    mv $1 $1.$DATE
+    mv $HOME/$1 $HOME/$1.$DATE
   else
     echo "Don't exists, $1"
   fi
@@ -27,11 +24,19 @@ done
 CURRENT_PATH=`pwd`
 
 CreateLinkTo() {
+  echo "Creating link to $1"
   $(cd $HOME && ln -s $CURRENT_PATH/dotfiles/$1 $1)
+  ls -la $HOME | grep $1
 }
 
-CreateLinkTo .tmux.conf
-CreateLinkTo .bashrc
+for i in $DOT_FILES_TO_BACKUP
+do
+ CreateLinkTo "$i"
+done
 
-source $TMUX_CONF
-source $BASHRC
+for i in $DOT_FILES_TO_BACKUP
+do
+ source ~/"$i"
+done
+
+
