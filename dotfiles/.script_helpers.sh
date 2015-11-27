058cfae4edbf5ea517843b06253ac78544657724
Diego Alves <diego.oborba@gmail.com>
2015-11-27 00:27:13 -0200

    Add new color and method to call it

--- dotfiles/.script_helpers.sh
+++ dotfiles/.script_helpers.sh
@@ -1,10 +1,15 @@
 txtblue='\e[0;36m' # Blue
+txtyellow='\e[0;33m' # Green
 txtgreen='\e[0;32m' # Green
 txtred='\e[0;31m' # Red
 clroff='\e[0m' # Text Reset
 
 echo_red() {
     echo -e "$txtred$1$clroff"
+}
+
+echo_yellow() {
+    echo -e "$txtyellow$1$clroff"
 }
 
 echo_green() {
